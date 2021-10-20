8678d84385fdec22b91759963135c8642208d79e
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2021-10-20 17:27:53 +0200

    subo algoritmos de ordenación

--- algoritmosOrdenacion/index.php
+++ algoritmosOrdenacion/index.php
@@ -6,6 +6,9 @@
     </head>
     <body>
         <?php
+        
+        //const POS1 = 2;
+        
         if (filter_input(INPUT_GET, "submit")) {
             $numeros = explode(",", filter_input(INPUT_GET, "numeros")); // split string to an Array
 
@@ -15,6 +18,8 @@
         } else {
             //echo displayForm();
             ordenacionBurbuja();
+            insercionDirecta();
+            seleccionDirecta();
         }
 
         /**
@@ -37,9 +42,10 @@
          * 
          */
         function ordenacionBurbuja() {
-            $array = [34, 45, 78, 1, 2, 0, 100, 324, 23];
             $cont = $aux = 0; //contador y auxiliar para controlar la ordenación
+            $array = [34, 45, 78, 1, 2, 0, 100, 324, 23];
 
+            echo "<h2>ORDENACIÓN BURBUJA</h2>";
             echo "array inicial sin ordenar: ";
             var_dump($array);
             echo "</br></br>";
@@ -75,6 +81,84 @@
                 $aux++;
             }
         }
+
+        /**
+         * Ordenación de una array método inserción Directa
+         * 
+         */
+        function insercionDirecta() {
+            $array = [12, 5, 780, 10, 29, 0, 120, 24, 3];
+            $newArray = [];
+
+            echo "<h2>INSERCIÓN DIRECTA</h2>";
+            echo "array inicial sin ordenar: ";
+            var_dump($array);
+            echo "</br></br>";
+
+            for ($i = 0; $i < count($array); $i++) {
+
+                if ($i === 0) {
+                    $newArray[] = $array[$i];
+                } else {
+                    cargarNumID($newArray, $array[$i]);
+                }
+            }
+
+            echo "array ordenada: ";
+            var_dump($newArray);
+        }
+
+        /**
+         * 
+         * Función para añadir 
+         * 
+         * @param type $newArray nueva array ordenada
+         * @param type $num numero a insertar
+         */
+        function cargarNumID(&$newArray, $num) {
+            $j = 0;
+            $cargado = false;
+
+            while (!$cargado && $j < count($newArray)) {
+
+                if ($newArray[$j] > $num) {
+
+                    array_splice($newArray, $j, 0, $num);
+                    $cargado = true;
+                } else if ($j === count($newArray) - 1) {
+                    array_splice($newArray, count($newArray), 0, $num);
+                    $cargado = true;
+                }
+
+                $j++;
+            }
+        }
+        
+        function seleccionDirecta(){
+            $array = [121, 35, 80, 0, 259, 10, 20, 4, 1];
+            $min = $pos = "";
+                        
+            echo "<h2>SELECCIÓN DIRECTA</h2>";
+            echo "array inicial sin ordenar: ";
+            var_dump($array);
+            echo "</br></br>";
+            
+            for($i = 0; $i < count($array); $i++) {
+                
+                $array_aux = array_slice($array, $i); //me quedo con la parte del array a buscar el min
+                $min = min($array_aux); //valor minimo
+                $pos = array_search($min, $array); //ubicación valor minimo
+                
+                array_splice($array, $pos, 1); //elimino elemento en su pos actual
+                array_splice($array, $i, 0, $min); //añado numero min en pos actual
+
+            }
+            
+            echo "array ordenada: ";
+            var_dump($array);
+        }
+        
+        
         ?>
 
     </body>
