d5261babaf38acddf62668868dae832c426c232c
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2021-10-20 15:29:58 +0200

    subo practica agenda

---
+++ agenda/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Practica agenda</title>
+    </head>
+    <body>
+        <?php
+
+        $nombre = $telefono = $agenda = "";
+
+        if (filter_input(INPUT_GET, "submit")) {
+            global $nombre, $telefono, $agenda;
+            $nombre = filter_input(INPUT_GET, "nombre");
+            $telefono = filter_input(INPUT_GET, "telefono");
+
+            //valido que el usuario haya introducido el nombre del nuevo contacto a registrar
+            if (empty($nombre)) {
+                echo "<h4 style='color:red;'>¡ATENCIÓN!</h4><p>El nombre es obligatorio.</p>";
+                
+            } else {
+
+                //busco si el nombre ya existe en la agenda
+                if (strpos(filter_input(INPUT_GET, "agenda"), "$nombre:") !== false) {
+                    $agenda = checkNombre($nombre, $telefono);
+                    
+                } else if (!empty($telefono)) {
+                    
+                    //si el nombre no existe en la agenda y se ha indicado un teléfono, lo añado
+                    $agenda = filter_input(INPUT_GET, "nombre") . ":" . filter_input(INPUT_GET, "telefono") . ",";
+
+                    //si ya tengo algun contacto incluido a la agenda, lo concateno
+                    if (!empty(filter_input(INPUT_GET, "agenda"))) {
+                        $agenda .= filter_input(INPUT_GET, "agenda");
+                    }
+                    
+                } else {
+                    echo "<h4 style='color:red;'>¡ATENCIÓN!</h4>"
+                    . "<ul><li>Para añadir un nuevo contacto, por favor indica un teléfono.</li>"
+                    . "<li>Para aliminar un contacto ya existente, por favor, escribe el nombre tal cual está registrado en la agenda</li>"
+                    . "<li>Para actualizar un teléfono, por favor, escribe el nombre tal cual está registrado y el nuevo número de teléfono.</li></ul>";
+                }
+            }
+
+            displayForm();
+            displayContacts();
+        } else {
+            displayForm();
+            echo "<p>No hay contactos en la agenda.</p>";
+        }
+
+        /**
+         * Impresión del formulario de registro
+         * 
+         * @global type $nombre
+         * @global type $telefono
+         * @global type $agenda listado total de contactos
+         */
+        function displayForm() {
+            global $nombre, $telefono, $agenda;
+
+            $form = "<form>" .
+                    "<input type='text' name='nombre' placeholder='nombre' value=$nombre >" .
+                    "<input type='number' name='telefono' placeholder='teléfono' value=$telefono >" .
+                    "<input type='hidden' name='agenda' value=$agenda >" .
+                    "<input type='submit' name='submit' value='insertar' > " .
+                    "<h2>AGENDA</h2>" .
+                    "</form>";
+
+            echo $form;
+        }
+
+        /**
+         * Lista de los contactos de la agenda
+         * 
+         * @global type $agenda listado de contactos
+         */
+        function displayContacts() {
+            global $agenda;
+            $contactos = explode(",", $agenda);
+            $list = "<ul>";
+
+            foreach ($contactos as $val) {
+                if (strlen($val) > 0) {
+                    $list .= "<li> $val </li>";
+                }
+            }
+
+            //explode retorna 1 cuando el string está vacío, por lo que en la primera posición siempre tengo
+            //+ info en https://stackoverflow.com/questions/36648491/why-does-explode-on-null-return-1-element
+            if (count($contactos) === 1) {
+                $list = "<p>No hay contactos en agenda.</p>";
+            }
+
+            $list .= "</ul>";
+
+            echo $list;
+        }
+
+        /**
+         * 
+         * Validación del nombre introducido en la agenda,
+         * si el $tlf viene vacío, se elimina el contacto,
+         * si tiene valor, se reemplaza en el contacto ya existente
+         * 
+         * @param type $nombre nuevo nombre introducido
+         * @param type $tlf nuevo teléfono introducido
+         * @return string agenda seteada
+         */
+        function checkNombre($nombre, $tlf) {
+            $new_agenda = "";
+            $i = 0; //contador
+            $contactos = explode(",", filter_input(INPUT_GET, "agenda"));
+            array_pop($contactos); //elimino el último elemento, ya que al hacer explode, el ultimo es un string vacío
+
+
+            while ($i < count($contactos)) {
+                $contacto = explode(":", $contactos[$i]);
+
+                if ($contacto[0] === $nombre && strlen($tlf) === 0) {
+                    //elimino el contacto de la agenda
+                    echo "¡Contacto eliminado!";
+                } else if ($contacto[0] === $nombre && strlen($tlf) > 0) {
+                    //seteo el valor del tlf de este contacto
+                    $contacto[1] = $tlf;
+                    $new_agenda .= $contacto[0] . ":" . $contacto[1] . ",";
+                } else {
+                    $new_agenda .= $contacto[0] . ":" . $contacto[1] . ",";
+                }
+
+                $i++;
+            }
+
+            return $new_agenda;
+        }
+        ?>
+
+    </body>
+</html>
--- algoritmosOrdenacion/index.php
+++ algoritmosOrdenacion/index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -10,33 +6,74 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     </head>
     <body>
         <?php
-        /*
-         * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-         * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
-         */
-
-        
         if (filter_input(INPUT_GET, "submit")) {
-            $numeros = explode (",", filter_input(INPUT_GET, "numeros")); // split string to an Array
-            
+            $numeros = explode(",", filter_input(INPUT_GET, "numeros")); // split string to an Array
+
             foreach ($numeros as $num) {
                 echo (int) $num;
-            }   
-            
+            }
         } else {
-            displayForm();
+            //echo displayForm();
+            ordenacionBurbuja();
         }
-        
-        function displayForm() {
-            ?>
-            <h1>Algoritmos de ordenación</h1>
-            <form>
-                <label for="numeros">Por favor, introduce un listado de números enteros separados por coma.</label>
-                <input type="text" name="numeros" id="numeros" />
-                <input type="submit" name="submit" />
-            </form>
 
-            <?php
+        /**
+         * 
+         * @return string formulario para añadir numeros manualmente
+         */
+        function displayForm() {
+            $output = "<h1>Algoritmos de ordenación</h1><h2>Burbuja</h2>" .
+                    "<form>" .
+                    "<label for='numeros'>Por favor, introduce un listado de números enteros separados por coma.</label></br>" .
+                    "<input type='text' name='numeros' id='numeros' />" .
+                    "<input type='submit' name='submit' />" .
+                    "</form>";
+
+            return $output;
+        }
+
+        /**
+         * Ordenación de un array método burbuja
+         * 
+         */
+        function ordenacionBurbuja() {
+            $array = [34, 45, 78, 1, 2, 0, 100, 324, 23];
+            $cont = $aux = 0; //contador y auxiliar para controlar la ordenación
+
+            echo "array inicial sin ordenar: ";
+            var_dump($array);
+            echo "</br></br>";
+
+            do {
+                $aux = 0;
+                for ($i = 0; $i < count($array); $i++) {
+                    if ($i < count($array) - 1) {
+                        giroValor($aux, $array, $i);
+                    }
+                }
+
+                $cont++;
+            } while ($aux > 0 && $cont < count($array));
+
+            echo "array ordenada método burbuja: ";
+            var_dump($array);
+        }
+
+        /**
+         * Intercambia el valor entre dos posiciones
+         * 
+         * @param type $aux auxiliar para controlar si se producen giros
+         * @param type $array lista de numeros a ordenar
+         * @param type $i puntero que indica la posicion en $array
+         */
+        function giroValor(&$aux, &$array, $i) {
+            if ($array[$i] > $array[$i + 1]) {
+                $mayor = $array[$i];
+
+                $array[$i] = $array[$i + 1];
+                $array[$i + 1] = $mayor;
+                $aux++;
+            }
         }
         ?>
 
